b9f5130de6af7bc3921a7715801022d6bc7b702b
fran <l.francisco.bj@gmail.com>
2024-09-17 16:44:49 -0600

    Añadiendo el modelado de los usuarios

--- src/main/java/com/example/userservice/Entity/User.java
+++ src/main/java/com/example/userservice/Entity/User.java
@@ -1,4 +1,44 @@
 package com.example.userservice.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUser;
+    @Column(nullable = false)
+    private String firstName;
+    private String middleName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private LocalDate birthDate;
+    @Column(nullable = false)
+    private String position;
+
+    public User(String firstName, String middleName, String lastName, LocalDate birthDate, String position) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.position = position;
+    }
+    public User(String firstName, String lastName, LocalDate birthDate, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.position = position;
+    }
 }
