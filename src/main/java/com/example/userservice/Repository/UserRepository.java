1df83a3c22eee8b7456aff61606e68bc9f7c0853
fran <l.francisco.bj@gmail.com>
2024-09-17 17:11:02 -0600

    Añadiendo consultas a la base de datos para validacion por identidad

--- src/main/java/com/example/userservice/Repository/UserRepository.java
+++ src/main/java/com/example/userservice/Repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.example.userservice.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByFirstNameAndAndMiddleNameAndLastNameAndBirthDate(String firstName, String middleName, String lastName, String birthDate);
-
+    Boolean existsByFirstNameAndMiddleNameAndLastNameAndBirthDate(String firstName, String middleName, String lastName, LocalDate birthDate);
+    Boolean existsByFirstNameAndLastNameAndBirthDate(String firstName, String lastName, LocalDate birthDate);
 }
