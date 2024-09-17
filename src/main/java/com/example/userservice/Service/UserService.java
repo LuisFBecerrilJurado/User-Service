a2552af12d8752fc451ab6cd98fcf7709d2f4b91
fran <l.francisco.bj@gmail.com>
2024-09-17 17:25:04 -0600

    Añadiendo metodos para Añadir, Buscar, Obtener, Eliminar Usuarios

--- src/main/java/com/example/userservice/Service/UserService.java
+++ src/main/java/com/example/userservice/Service/UserService.java
@@ -1,4 +1,14 @@
 package com.example.userservice.Service;
 
+import com.example.userservice.Entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long idUser);
+    void saveUser(User user);
+    Optional<User> updateUser(User user);
+    void deleteUserById(Long idUser);
 }
--- src/main/java/com/example/userservice/Service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/Service/UserServiceImpl.java
@@ -1,4 +1,68 @@
 package com.example.userservice.Service;
 
-public class UserServiceImpl {
+import com.example.userservice.Entity.User;
+import com.example.userservice.Exceptions.UserExceptions;
+import com.example.userservice.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long idUser) {
+        return userRepository.findById(idUser).orElseThrow(()-> new UserExceptions("User Not Found with id: "+ idUser));
+    }
+
+    @Override
+    public void deleteUserById(Long idUser) {
+        userRepository.deleteById(idUser);
+    }
+
+    public boolean ageValidation(LocalDate birthDate){
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(birthDate, today);
+        return period.getYears() >= 18;
+    }
+
+    public boolean userValidation(User user){
+        if(user.getMiddleName().isEmpty()){
+            return (userRepository.existsByFirstNameAndLastNameAndBirthDate(user.getFirstName(), user.getLastName(), user.getBirthDate()));
+        }
+        return(userRepository.existsByFirstNameAndMiddleNameAndLastNameAndBirthDate(user.getFirstName(), user.getMiddleName(), user.getLastName(), user.getBirthDate()));
+    }
+
+    @Override
+    public void saveUser(User user) {
+        boolean ageValid = ageValidation(user.getBirthDate());
+        boolean userValid = userValidation(user);
+        if(!userValid) throw new UserExceptions("User already exists in system");
+        if (!ageValid) throw new UserExceptions("Age not valid");
+        userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> updateUser(User user) {
+        Optional<User> userOptional = userRepository.findById(user.getIdUser());
+        if(userOptional.isPresent()){
+            boolean ageValid = ageValidation(user.getBirthDate());
+            boolean userValid = userValidation(user);
+            if(!userValid) throw new UserExceptions("User already exists in system");
+            if (!ageValid) throw new UserExceptions("Age not valid");
+            userRepository.save(user);
+        }
+        throw new UserExceptions("User not found in system");
+    }
 }
