b6786d9de008d3ff25d130756e8392757323515e
fran <l.francisco.bj@gmail.com>
2024-09-17 16:48:27 -0600

    Añadiendo el repositorio para consulta a base de datos

---
+++ src/main/java/com/example/userservice/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.userservice.Repository;
+
+import com.example.userservice.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByFirstNameAndAndMiddleNameAndLastNameAndBirthDate(String firstName, String middleName, String lastName, String birthDate);
+
+}
