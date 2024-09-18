4c61be5d5a701eb341d8602ffee6f518bfdc689b
fran <l.francisco.bj@gmail.com>
2024-09-17 19:49:59 -0600

    [FIX] Corrigiendo el tipo de variable de la ID

--- src/main/java/com/example/userservice/Repository/UserRepository.java
+++ src/main/java/com/example/userservice/Repository/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByFirstNameAndMiddleNameAndLastNameAndBirthDate(String firstName, String middleName, String lastName, LocalDate birthDate);
     Boolean existsByFirstNameAndLastNameAndBirthDate(String firstName, String lastName, LocalDate birthDate);
 }
