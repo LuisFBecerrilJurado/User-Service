37c674262465101fe8db5389db370fed1e61032f
fran <l.francisco.bj@gmail.com>
2024-09-17 19:47:07 -0600

    Añadiendo Manejo para las excepciones

---
+++ src/main/java/com/example/userservice/Exceptions/GlobalExceptions.java
@@ -0,0 +1,20 @@
+package com.example.userservice.Exceptions;
+import com.example.userservice.Response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptions {
+    @ExceptionHandler(UserExceptions.class)
+    public ResponseEntity<ApiResponse> handlerNotFound(UserExceptions ue){
+        String message = ue.getMessage();
+        ApiResponse response = new ApiResponse().builder()
+                .message(message)
+                .success(true)
+                .status(HttpStatus.BAD_REQUEST)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
