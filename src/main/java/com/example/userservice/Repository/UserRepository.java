44d8e8f59d7bb0640fb58131a73f6c7a6af13931
fran <l.francisco.bj@gmail.com>
2024-09-19 19:01:23 -0600

    Remove redundant method from UserRepository
    Eliminated the unnecessary `existsByFirstNameAndLastNameAndBirthDate` method to simplify the UserRepository interface. The remaining method provides similar functionality, ensuring codebase cleanliness and maintainability.

--- src/main/java/com/example/userservice/Repository/UserRepository.java
+++ src/main/java/com/example/userservice/Repository/UserRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByFirstNameAndMiddleNameAndLastNameAndBirthDate(String firstName, String middleName, String lastName, LocalDate birthDate);
-    Boolean existsByFirstNameAndLastNameAndBirthDate(String firstName, String lastName, LocalDate birthDate);
 }
