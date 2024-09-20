d2569b6af7ceaafc0da78b1f9475ae8f45c70b86
fran <l.francisco.bj@gmail.com>
2024-09-19 18:51:11 -0600

    Refactor UserService interface methods
    Changed return types for CRUD methods in UserService interface. Now using Optional for getUserById and returning void for saveUser, updateUser, and deleteUserById.

--- src/main/java/com/example/userservice/Service/UserService.java
+++ src/main/java/com/example/userservice/Service/UserService.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface UserService {
     List<User> getAllUsers();
-    User getUserById(String idUser);
-    User saveUser(User user);
-    Optional<User> updateUser(User user);
-    User deleteUserById(String idUser);
+    Optional<User>  getUserById(String idUser);
+    void saveUser(User user);
+    void updateUser(User user);
+    void deleteUserById(String idUser);
 }
